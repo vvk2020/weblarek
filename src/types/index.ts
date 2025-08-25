c2181ddaeea1c209953a30cf3cc35657876c06e2
V Kuznetsov <vvsmith13@gmail.com>
2025-08-25 21:19:48 +0300

    A1.02

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,46 +5,6 @@ export interface IApi {
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
 
-
-// /**
-//  * ТОВАР
-//  * @property {string} id - uuid товара ("854cef69-976d-4c2a-a18c-2aa45046c390"),
-//  * @property {string} description - описание ("Если планируете решать задачи в тренажёре, берите два."),
-//  * @property {string} image - картинка ("/5_Dots.svg")
-//  * @property {string} title - название ("+1 час в сутках")
-//  * @property {string} category - категория ("софт-скил")
-//  * @property {number | null} price - цена (750 | null)
-//  */
-// interface ICard {
-//   id: string; // уникальный идентификатор товара ("854cef69-976d-4c2a-a18c-2aa45046c390"),
-//   description: string; // Описание ("Если планируете решать задачи в тренажёре, берите два."),
-//   image: string; // картинка ("/5_Dots.svg")
-//   title: string; // название ("+1 час в сутках")
-//   category: string; // категория ("софт-скил")
-//   price: number | null; // цена (750 | null)
-// }
-
-type TPayment = "online" | "При получении";
-
-// /**
-//  * ЗАКАЗ
-//  * @property {string} [id] - uuid заказа ("28c57cb4-3002-4445-8aa1-2a06a5055ae5")юНазначается после оформления заказа
-//  * @property {PaymentMethod} payment - способ оплаты ("online" | ???)
-//  * @property {string} email - почта заказчика ("test@test.ru")
-//  * @property {string} phone - телефон заказчика (+71234567890")
-//  * @property {number | null} total - сумма заказа (2200)
-//  * @property {ICard[]} items - массив товаров (для API - массив UUID товаров (["854cef69-976d-4c2a-a18c-2aa45046c390","c101ab44-ed99-4a54-990d-47aa2bb4e7d9"])
-//  * 
-//  */
-// interface IOrder {
-//   id?: string, //28c57cb4-3002-4445-8aa1-2a06a5055ae5 - уникальный идентификатор заказа
-//   payment: PaymentMethod, // способ оплаты ("online")
-//   email: string, // почта заказчика ("test@test.ru")
-//   phone: string, // телефон заказчика (+71234567890")
-//   total: number | null, // сумма заказа (2200)
-//   items: ICard[] // массив товаров (для API - массив UUID товаров (["854cef69-976d-4c2a-a18c-2aa45046c390","c101ab44-ed99-4a54-990d-47aa2bb4e7d9"])
-// }
-
  /** ТОВАР
  * @property {string} id - uuid товара ("854cef69-976d-4c2a-a18c-2aa45046c390"),
  * @property {string} description - описание ("Если планируете решать задачи в тренажёре, берите два."),
@@ -54,13 +14,18 @@ type TPayment = "online" | "При получении";
  * @property {number | null} price - цена (750 | null)
  */
 export interface IProduct {
-  id: string;
-  description: string;
-  image: string;
-  title: string;
-  category: string;
-  price: number | null;
+id: string;
+description: string;
+image: string;
+title: string;
+category: string;
+price: number | null;
 }
+
+/**
+ * СПОСОб ОПЛАТЫ
+ */
+export type TPayment = "online" | "При получении";
 
 /**
  * ПОКУПАТЕЛЬ
@@ -68,8 +33,8 @@ export interface IProduct {
  * @property {PaymentMethod} payment - способ оплаты ("online" | ???)
  * @property {string} email - почта заказчика ("test@test.ru")
  * @property {string} phone - телефон заказчика (+71234567890")
- * @property {number | null} total - сумма заказа (2200)
- * @property {ICard[]} items - массив товаров (для API - массив UUID товаров (["854cef69-976d-4c2a-a18c-2aa45046c390","c101ab44-ed99-4a54-990d-47aa2bb4e7d9"])
+ * @property {string} address - адрес доставки
+ * 
  * 
  */
 export interface IBuyer {
@@ -78,3 +43,39 @@ export interface IBuyer {
   phone: string;
   address: string;
 }
+
+/* ПРОИЗВОДНЫЕ ТИПЫ ДАННЫХ ОТ IProduct, ИСПОЛЬЗУЕМЫЕ В... */
+
+/**
+ * ОПИСАНИЕ ТОВАРА
+ * (карточки товаров в галереи)
+ */
+export type GaleryCardData = Omit<IProduct, 'description'>;
+
+// ... карточках при их просмотре в отдельном окне полностью совпадает с IProduct
+
+/**
+ * ОПИСАНИЕ И ССЫЛКА НА ИЗОБРАЖЕНИЕ ТОВАРА
+ * (карточки товаров в корзине)
+ */
+export type BasketCardData = Omit<IProduct, 'description' | 'image'>;
+
+/* ПРОИЗВОДНЫЕ ТИПЫ ДАННЫХ ОТ IBuyer, ИСПОЛЬЗУЕМЫЕ В... */
+
+/**
+ * АДРЕС ДОСТАВКИ
+ * (первое окно оформления заказа)
+ */
+export type Address = Pick<IBuyer, 'address'>;
+
+/**
+ * ЭЛЕКТРОННАЯ ПОЧТА
+ * (второе окно оформления заказа)
+ */
+export type Email = Pick<IBuyer, 'email'>;
+
+/**
+ * ТЕЛЕФОН
+ * (второе окно оформления заказа)
+ */
+export type Phone = Pick<IBuyer, 'phone'>;
