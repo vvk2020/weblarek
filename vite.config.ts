a2e38798e243a77a6821e8cab8b18aca96fcfe1a
V Kuznetsov <vvsmith13@gmail.com>
2025-09-18 08:54:32 +0300

    pr9-rс5.42

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    hmr: {
-      overlay: true // Показывать уведомления об ошибках HMR
-    },
-    watch: {
-      usePolling: true,
-      interval: 100
-    }
-  },
-  build: {
-    sourcemap: true // Для отладки
-  },
   css: {
     preprocessorOptions: {
       scss: {
